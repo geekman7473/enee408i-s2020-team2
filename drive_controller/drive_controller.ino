889a9963ab1bed70d40be587e80348e56c161e83
Justin Becker <geekman7473@gmail.com>
2020-02-02 16:52:28 -0500

    Added PID to drive_controller

--- drive_controller/drive_controller.ino
+++ drive_controller/drive_controller.ino
@@ -1,6 +1,30 @@
+#include <PID_v1.h>
+
+#define LEFT_MOTOR_PWM 6
+#define RIGHT_MOTOR_PWM 7
+
+double leftMotorSetpoint, rightMotorSetpoint, leftMotorSpeed, rightMotorSpeed;
+uint8_t leftMotorPWM, rghtMotorPWM;
+int leftKp = 2, rightKp = 2, leftKi = 5, rightKi = 5, leftKd = 0, rightKd = 0;
+
+PID leftMotorPID(&leftMotorSpeed, &leftMotorPWM, &leftMotorSetpoint, leftKp, leftKi, leftKd, DIRECT);
+PID rightMotorPID(&rightMotorSpeed, &rightMotorPWM, &rightMotorSetpoint, rightKp, rightKi, rightKd, DIRECT);
 
 void setup() {
+  leftMotorSetpoint = 0;
+  rightMotorSetpoint = 0;
+
+  leftMotorPID.SetMode(AUTOMATIC);
+  rightMotorPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
+  leftMotorSpeed = 0; //TODO: Read from I2C
+  rightMotorSpeed = 0; //TODO: Read from I2C
+  
+  leftMotorPID.Compute();
+  rightMotorPID.Compute();
+
+  analogWrite(LEFT_MOTOR_PWM, leftMotorPWM);
+  analogWrite(RIGHT_MOTOR_PWM, rightMotorPWM);
 }
