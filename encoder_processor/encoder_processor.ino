80862d434163b67c1b00765cde834dc23c4aee08
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2020-02-02 14:32:28 -0500

    Workingish print to serial

--- encoder_processor/encoder_processor.ino
+++ encoder_processor/encoder_processor.ino
@@ -1,6 +1,68 @@
+#include<Wire.h>
+#include<Encoder.h>
+
+const uint8_t I2C_ADDRESS = 0x042;
+const uint8_t ENC_PIN1 = 5;
+const uint8_t ENC_PIN2 = 6;
+
+const uint8_t REGISTER_COUNT = 0x0;
+const uint8_t REGISTER_VELOCITY = 0x1;
+const uint8_t REGISTER_RESET = 0x2;
+uint8_t opcode;
+Encoder encoder(ENC_PIN1, ENC_PIN2);
+typedef union reading {
+  byte b[4];
+  float fval;
+} reading;
+reading rpm;
+
+volatile int test = 0;
 
 void setup() {
+  Wire.begin(I2C_ADDRESS);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
 }
 
-void loop() {
+void receiveEvent(int bytes) {
+  opcode = Wire.read();
+
+  if(bytes > 1)
+  {
+    if(opcode == REGISTER_RESET)
+    {
+      encoder.write(0);
+    }
+  }
 }
+
+void requestEvent() {
+  switch(opcode)
+  {
+   case REGISTER_COUNT:
+     int count = encoder.read();
+     Wire.write(count);
+  }
+}
+
+void loop(){
+  // Spin forever
+  Serial.println(encoder.read());
+}
+
+// Speed timer setup:
+void speed_setup()
+{
+  // Write TOP:
+  TCA0.SINGLE.PER = 0xFFFF;
+  // Setup the prescaler and enable:
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc | TCA_SINGLE_ENABLE_bm;
+  //TCB0.CTRLA |= TCB_ENABLE|TCB_CLKSEL_ 
+}
+
+ISR(TCA0_OVF_vect)
+{
+  test++;
+}
+
