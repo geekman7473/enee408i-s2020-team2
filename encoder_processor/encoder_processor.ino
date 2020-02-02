3380f2bc04212716e71aa1fffd58703b71ea26f6
Justin Becker <geekman7473@gmail.com>
2020-02-02 15:58:51 -0500

    Updated encoder processor to handle two encoders at once

--- encoder_processor/encoder_processor.ino
+++ encoder_processor/encoder_processor.ino
@@ -2,14 +2,19 @@
 #include "Encoder.h"
 
 const uint8_t I2C_ADDRESS = 0x042;
-const uint8_t ENC_PIN1 = 17;
-const uint8_t ENC_PIN2 = 7;
+const uint8_t ENC_1_PIN1 = 17;
+const uint8_t ENC_1_PIN2 = 7;
+const uint8_t ENC_2_PIN1 = 18;
+const uint8_t ENC_2_PIN2 = 6;
 
 const uint8_t REGISTER_COUNT = 0x0;
 const uint8_t REGISTER_VELOCITY = 0x1;
 const uint8_t REGISTER_RESET = 0x2;
 uint8_t opcode;
-Encoder encoder(ENC_PIN1, ENC_PIN2);
+
+Encoder encoder_left(ENC_1_PIN1, ENC_1_PIN2);
+Encoder encoder_right(ENC_2_PIN1, ENC_2_PIN2);
+
 typedef union reading {
   byte b[4];
   float fval;
@@ -22,7 +27,8 @@ void setup() {
   //Wire.begin(I2C_ADDRESS);
   //Wire.onRequest(requestEvent);
   //Wire.onReceive(receiveEvent);
-  encoder.write(0);
+  encoder_left.write(0);
+  encoder_right.write(0);
   Serial.begin(9600);
   //speed_setup();
 }
@@ -34,7 +40,7 @@ void receiveEvent(int bytes) {
   {
     if(opcode == REGISTER_RESET)
     {
-      encoder.write(0);
+      encoder_left.write(0);
     }
   }
 }
@@ -43,14 +49,16 @@ void requestEvent() {
   switch(opcode)
   {
    case REGISTER_COUNT:
-     int count = encoder.read();
+     int count = encoder_left.read();
      Wire.write(count);
   }
 }
 
 void loop(){
   // Spin forever
-  Serial.println(encoder.read());
+  Serial.print(encoder_left.read());
+  Serial.print(" ");
+  Serial.println(encoder_right.read());
   //Serial.print(test);
 }
 
