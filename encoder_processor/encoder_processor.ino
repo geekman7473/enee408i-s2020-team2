0fa44d2d39a0975f151aeb8639a218df76ec87a2
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2020-02-03 18:42:37 -0500

    Working timer interrupt. TODO: calculate frequency and speed calculations

--- encoder_processor/encoder_processor.ino
+++ encoder_processor/encoder_processor.ino
@@ -1,78 +1,150 @@
 #include<Wire.h>
 #include "Encoder.h"
+#include<Arduino.h>
 
 const uint8_t I2C_ADDRESS = 0x042;
 const uint8_t ENC_1_PIN1 = 17;
 const uint8_t ENC_1_PIN2 = 7;
-const uint8_t ENC_2_PIN1 = 18;
+const uint8_t ENC_2_PIN1 = 16;
 const uint8_t ENC_2_PIN2 = 6;
 
-const uint8_t REGISTER_COUNT = 0x0;
-const uint8_t REGISTER_VELOCITY = 0x1;
-const uint8_t REGISTER_RESET = 0x2;
+const uint8_t REGISTER_COUNT_LEFT = 0x01;
+const uint8_t REGISTER_COUNT_RIGHT = 0x02;
+const uint8_t REGISTER_VELOCITY_LEFT = 0x11;
+const uint8_t REGISTER_VELOCITY_RIGHT = 0x12;
+const uint8_t REGISTER_RESET_LEFT = 0x21;
+const uint8_t REGISTER_RESET_RIGHT = 0x22;
+const uint8_t REGISTER_RESET_BOTH = 0x23;
+
 uint8_t opcode;
 
 Encoder encoder_left(ENC_1_PIN1, ENC_1_PIN2);
 Encoder encoder_right(ENC_2_PIN1, ENC_2_PIN2);
 
-typedef union reading {
-  byte b[4];
+typedef union int32_i2c {
+  byte buf[4];
+  int32_t ival;
+} int32_i2c_t;
+
+typedef union float_i2c {
+  byte buf[4];
   float fval;
-} reading;
-reading rpm;
+} float_i2c_t;
+
+volatile int32_i2c_t prev_left;
+volatile int32_i2c_t prev_right;
+
+volatile float_i2c_t speed_left;
+volatile float_i2c_t speed_right;
 
 volatile int test = 0;
 
-void setup() {
-  //Wire.begin(I2C_ADDRESS);
-  //Wire.onRequest(requestEvent);
-  //Wire.onReceive(receiveEvent);
+void setup()
+{
+  Wire.begin(I2C_ADDRESS);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
   encoder_left.write(0);
   encoder_right.write(0);
+  prev_left.ival = 0;
+  prev_right.ival = 0;
   Serial.begin(9600);
-  //speed_setup();
+  speed_setup();
 }
 
-void receiveEvent(int bytes) {
+void receiveEvent(int bytes)
+{
   opcode = Wire.read();
 
   if(bytes > 1)
   {
-    if(opcode == REGISTER_RESET)
+    int32_i2c_t val;
+    switch(opcode)
     {
-      encoder_left.write(0);
+     case REGISTER_RESET_LEFT:
+       for(int i = 0; i < 4; i++)
+       {
+         val.buf[i] = Wire.read();
+       }
+       encoder_left.write(val.ival);
+       break;
+
+     case REGISTER_RESET_RIGHT:
+       for(int i = 0; i < 4; i++)
+       {
+         val.buf[i] = Wire.read();
+       }
+       encoder_right.write(val.ival);
+       break;
+
+     case REGISTER_RESET_BOTH:
+       for(int i = 0; i < 4; i++)
+       {
+         val.buf[i] = Wire.read();
+       }
+       encoder_left.write(val.ival);
+       encoder_right.write(val.ival);
+       break;
     }
   }
 }
 
-void requestEvent() {
+void requestEvent()
+{
+  int32_i2c_t count;
+ 
   switch(opcode)
   {
-   case REGISTER_COUNT:
-     int count = encoder_left.read();
-     Wire.write(count);
+   case REGISTER_COUNT_LEFT:
+     count.ival = encoder_left.read();
+     Wire.write(count.buf, sizeof(int32_i2c_t));
+     break;
+
+   case REGISTER_COUNT_RIGHT:
+     count.ival = encoder_right.read();
+     Wire.write(count.buf, sizeof(int32_i2c_t));
+     break;
   }
 }
 
-void loop(){
+void loop()
+{
   // Spin forever
-  Serial.print(encoder_left.read());
-  Serial.print(" ");
-  Serial.println(encoder_right.read());
+
+  //Serial.print(encoder_left.read());
+  //Serial.print(" ");
+  //Serial.println(encoder_right.read());
   //Serial.print(test);
+  /*
+  int32_t curr_left = encoder_left.get();
+  int32_t curr_right = encoder_right.get();
+
+  // Measure ticks per second:
+  int32_t dist_left = (curr_left - prev_left) * 100;
+  int32_t dist_right = (curr_right - prev_right) * 100;
+
+  delay(10);
+  */
+
+  //Serial.println(test);
+
+  delay(10000);
+  Serial.println(test);
 }
 
 // Speed timer setup:
 void speed_setup()
 {
   // Write TOP:
-  TCA0.SINGLE.PER = F_CPU / 1000 - 1;   // Overflow after 1 ms
-  TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;   // Enable overflow interrupt
-	TCA0.SINGLE.CTRLA = TCA_SINGLE_ENABLE_bm;   // Start without prescaler, at full CPU clock Speed
+  TCB2.CCMP =  250000; // Overflow after 10 ms using the TCA clk 
+  TCB2.INTCTRL = TCB_CAPT_bm;
+  TCB2.CTRLB = TCB_CNTMODE_INT_gc; // Setup timer into periodic interupt mode
+  TCB2.CTRLA = TCB_ENABLE_bm | TCB_CLKSEL_CLKTCA_gc; // Enable counter Using the TCA clock
+  Serial.println(F_CPU);
 }
 
-ISR(TCA0_OVF_vect)
+ISR(TCB2_INT_vect)
 {
-  test++;
-  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_OVF_bm;
+  test++; 
+  TCB2.INTFLAGS = TCB_CAPT_bm;
 }
