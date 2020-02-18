aacdb8494ac88d36bcb6c5a562b1e2915819b23f
Justin Becker <geekman7473@gmail.com>
2020-02-18 13:25:47 -0500

    Ultrasonic debugging

---
+++ ultrasonic_processor/Ultrasound.h
@@ -0,0 +1,21 @@
+#ifndef Ultrasound_h
+#define Ultrasound_h
+
+#include "Arduino.h"
+
+// Header file for Ultrasound.cpp
+
+#define DEFAULT_PULSEWIDTH 4000
+#define DEFAULT_PULSE_SCHEDULE 10          
+
+class Ultrasound
+{
+  public:
+    Ultrasound(int trigPin1, int echoPin1, int trigPin2, int echoPin2, int trigPin3, int echoPin3);
+    Ultrasound(int trigPin1, int echoPin1, int trigPin2, int echoPin2, int trigPin3, int echoPin3, int pulseWidth, int pulseSchedule);
+    double getDistance(int ultrasonicNum);
+    double microsecondsToInches(long microseconds);
+    double microsecondsToCentimeters(long microseconds);
+};
+
+#endif
--- ultrasonic_processor/ultrasonic_processor.ino
+++ ultrasonic_processor/ultrasonic_processor.ino
@@ -1,6 +1,17 @@
+#include <Wire.h>
+#include <SerialCommands.h>
 
+#include "Ultrasound.h"
+
+Ultrasound ultrasound(8, 2, 9, 3, 10, 4);
+ 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
+  Serial.print(ultrasound.getDistance(1));
+  Serial.print(" ");
+  Serial.println(ultrasound.getDistance(2));
+  delay(100);
 }
