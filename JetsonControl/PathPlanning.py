84d41aca5dee7b29e19fc9bd7a133475f4888c45
Anton Pozharskiy <apozharski@gmail.com>
2020-05-10 23:10:12 -0400

    Added straight line planning, driving at legos

--- JetsonControl/PathPlanning.py
+++ JetsonControl/PathPlanning.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import time
 from .DriveControlInterop import DriveController
 
-def plan_turn(theta, w, a, dt = .1, r = 14):
+# TODO make all this usefully packaged 
+def plan_turn(theta, w, a, direction, dt = .1, r = 14):
     t_ramp = w/a
     theta_min = w**2/a
     theta_rect = theta - theta_min
@@ -37,16 +38,60 @@ def plan_turn(theta, w, a, dt = .1, r = 14):
             curr_t +=dt
 
         control = control_w * r
-        
-    return control, t
 
-def run_plan(controller,control, direction = "right", dt=.1):
-    for v in control:
-        print(controller.read_speeds(0),controller.read_speeds(1))
-        if direction == "right":
-            controller.send_speed(v, -v)
-        elif direction == "left":
-            controller.send_speed(-v, v)
+    if direction=="right":
+        r_control = -control
+        l_control = control
+    elif direction=="left":
+        r_control = control
+        l_control = -control
+    return l_control, r_control, t
+
+# TODO ahh dammit v vs w. fix it.
+def plan_straight(dist, v, a, direction, dt=.1):
+    t_ramp = v/a
+    dist_min = v**2/a
+    dist_rect = dist - dist_min
+    if dist_rect > 0:
+        t_rect = dist_rect/v
+        t_total = 2*t_ramp + t_rect
+        
+        ramp_up = a * np.linspace(0, t_ramp, int(t_ramp/dt + 1))
+        ramp_down = v - (a * np.linspace(0, t_ramp, int(t_ramp/dt + 1)))
+        rect = np.full((int((t_rect/dt)+1)), v)
+        
+        control = np.concatenate([ramp_up[:-1], rect[:-1], ramp_down])
+        t = []
+        curr_t = 0
+        for _ in control:
+            t.append(curr_t)
+            curr_t +=dt
+    else:
+        t_ramp = np.sqrt(dist/a)
+        peak_v = a*t_ramp
+        ramp_up = a * np.linspace(0, t_ramp, int(t_ramp/dt + 1))
+        ramp_down =  peak_v - (a * np.linspace(0, t_ramp, int(t_ramp/dt + 1)))
+
+        control = np.concatenate([ramp_up[:-1], ramp_down])
+        t = []
+        curr_t = 0
+        for _ in control:
+            t.append(curr_t)
+            curr_t +=dt
+
+    if direction == "fwd":
+        l_control = control
+        r_control = control
+
+    if direction == "bwd":
+        l_control = -control
+        r_control = -control
+    return l_control, r_control, t
+
+def run_plan(controller, l_control, r_control, dt=.1):
+    for l,r in zip(l_control, r_control):
+        #print(controller.read_speeds(0),controller.read_speeds(1))
+        controller.send_speed(l, r)
         time.sleep(dt)
 
 
--- JetsonControl/tracking.py
+++ JetsonControl/tracking.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# BIG HUGE TODO: Clean this up
 from .models import *
 from .utils.utils import *
 from .sort import *
@@ -19,12 +20,13 @@ import numpy as np
 
 from PIL import Image
 
-from .PathPlanning import plan_turn, run_plan
+from .PathPlanning import *
+from .DriveControlInterop import DriveController
 
 # These config and weight files need to be changed to match your current model
-config_path='config/yolov3-tiny.cfg'
-weights_path='config/yolov3_ckpt_19.pth'
-class_path='config/coco.names'
+config_path='/home/robot/enee408i-s2020-team2/JetsonControl/config/yolov3-tiny.cfg'
+weights_path='/home/robot/enee408i-s2020-team2/JetsonControl/config/yolov3_ckpt_19.pth'
+class_path='/home/robot/enee408i-s2020-team2/JetsonControl/config/coco.names'
 img_size=416
 # I found that adjusting the confidence threshold was very useful in eliminating false positives
 conf_thres=0.80
@@ -40,7 +42,7 @@ Tensor = torch.cuda.FloatTensor
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 model.cuda()
 model.eval()
-classes = utils.load_classes(class_path)
+classes = load_classes(class_path)
 
 def detect_image(img):
     # scale and pad image
@@ -61,14 +63,34 @@ def detect_image(img):
     # run inference on the model and get detections
     with torch.no_grad():
         detections = model(input_img)
-        detections = utils.non_max_suppression(detections, conf_thres, nms_thres)
+        detections = non_max_suppression(detections, conf_thres, nms_thres)
     return detections[0]
-	
-# This you can adjust to any video file of your choice
-videopath = './videos/IMG_9663.mp4'
 
-#%pylab inline 
-#from IPython.display import clear_output
+def angleFromDetection(x1, x2, y1, y2, x_im, y_im):
+    FOV = 78
+    degrees_per_pixel = FOV / x_im;
+    midpoint_x = (x1 + x2) / 2
+    midpoint_dist = midpoint_x - (x_im / 2)
+    return (midpoint_dist * degrees_per_pixel) * (3.14159265358979 / 180)
+
+def draw(detections, tracked_objects):
+    #tracked_objects = mot_tracker.update(detections.cpu())
+    #
+    unique_labels = detections[:, -1].cpu().unique()
+    n_cls_preds = len(unique_labels)
+    for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
+        box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+        box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+        y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+        x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+        #
+        color = colors[int(obj_id) % len(colors)]
+        color = [i * 255 for i in color]
+        cls = classes[int(cls_pred)]
+        cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
+        cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
+        cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+
 
 cmap = plt.get_cmap('tab20b')
 colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
@@ -76,6 +98,8 @@ colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 # initialize Sort object and video capture
 vid = cv2.VideoCapture('/dev/video1')
 mot_tracker = Sort() 
+
+vid.set(cv2.CAP_PROP_FPS, 10)
 
 out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (1920,1080))
 
@@ -85,13 +109,13 @@ i = 0
 import time
 millis = int(round(time.time() * 1000))
 
-fig=plt.figure(figsize=(12, 8))
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
+#plt.ion()
+#plt.show()
 
-plt.ion()
-plt.show()
-
-while (vid.isOpened()):
+# get an angle to go at.
+angle = None
+while angle is None:
     print("FRAME")
     vid.grab()
     vid.grab()
@@ -105,29 +129,27 @@ while (vid.isOpened()):
     detections = detect_image(pilimg)
     #
     img = np.array(pilimg)
-    pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
-    pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
-    unpad_h = img_size - pad_y
-    unpad_w = img_size - pad_x
     if detections is not None:
-        print(detections)
-    if False:
-        tracked_objects = mot_tracker.update(detections.cpu())
-        #
-        unique_labels = detections[:, -1].cpu().unique()
-        n_cls_preds = len(unique_labels)
-        for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
-            box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
-            box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
-            y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
-            x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
-            #
-            color = colors[int(obj_id) % len(colors)]
-            color = [i * 255 for i in color]
-            cls = classes[int(cls_pred)]
-            cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-            cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
-            cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+        boxes = []
+        for x1, y1, x2, y2, _, _, _ in detections.cpu():
+            area = (x2-x1) * (y2 - y1)
+            #box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+            #box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+            #y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+            #x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+            
+            #color = colors[int(0) % len(colors)]
+            #color = [i * 255 for i in color]
+            #cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
+            boxes.append((area.item(), angleFromDetection(x1, x2, y1, y2, img.shape[0], img.shape[1]).item()))
+        boxes.sort()
+        print("boxes: " + str(boxes))
+        angle = boxes[0][1]
+    # pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
+    # pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
+    # unpad_h = img_size - pad_y
+    # unpad_w = img_size - pad_x
+
     i = i + 1
     print("FPS: " + str(i / ((int(round(time.time() * 1000)) - millis) / 1000)))
     #cv2.imshow('Frame', frame)
@@ -135,15 +157,25 @@ while (vid.isOpened()):
     # I elected to not output the results here in the notebook for performance,
     # but you can feel free to uncomment these lines if you want a live view of your model
     #out.write(frame)
-    ax = fig.gca()
+    #ax = fig.gca()
     #ax.title("Video Stream")
-    ax.imshow(frame)
-    plt.draw()
-    plt.pause(.0001)
+    #ax.imshow(frame)
+    #plt.draw()
+    #plt.pause(.0001)
     #img=cv2.imread('image.jpg',1)
     #you can use:
     #img2 = Image.fromarray(frame, 'RGB')
     #img2.show()
     #clear_output(wait=True)
+
+direction = "right" if angle > 0 else "left"
+l_turn, r_turn, _ = plan_turn(abs(angle), np.pi/2, 2, direction)
+l_straight, r_straight, _ = plan_straight(100, 25, 25, "fwd")
+
+controller = DriveController("/dev/ttyACM0")
+
+run_plan(controller, l_turn, r_turn)
+run_plan(controller, l_straight, r_straight)
 #out.release()
 vid.release()
+
